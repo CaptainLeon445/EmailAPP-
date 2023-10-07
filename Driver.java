241b943e43946770c3b456d39e202faf1b58172f
CaptainLeon445 <christopherleon237@gmail.com>
2023-10-07 08:44:46 +0100

    added get profile method

--- Driver.java
+++ Driver.java
@@ -1,6 +1,10 @@
 public class Driver{
     public static void main(String[] args){
-       EmailApp obj = new EmailApp("Chris", "leon"); 
-       obj.getEmail();
+       EmailApp obj = new EmailApp("Chris", "leon");
+       obj.setAlternateMail("leon.chris@new.mail.com");
+       obj.setMailBoxCapacity(200, "mb");
+       obj.getProfile();
+
+
     }
 }
--- EmailApp.java
+++ EmailApp.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 
 public class EmailApp{
    private String firstname; 
@@ -8,7 +11,7 @@ public class EmailApp{
    private String department; 
    private String company="testing"; 
    private String alternateEmail; 
-   private int mailCapacity; 
+   private String mailCapacity; 
    private int defaultPasswordlength = 10;
 
    public EmailApp(String firstname, String lastname) {
@@ -16,11 +19,8 @@ public class EmailApp{
     this.lastname = lastname;
     this.department = setDepartment();
     email = setEmail();
-    // password = setPassword();
+    password = setRandomPassword(defaultPasswordlength);
     
-
-
-
 
    }
 
@@ -47,9 +47,52 @@ public class EmailApp{
     return firstname.toLowerCase() +"."+ lastname.toLowerCase() + "@"+this.department+"."+company+".com";
    }
 
-   public void getEmail(){
-    System.out.println(email);
+   private String setRandomPassword (int length){
+     String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_";
+     char[] password = new char[length];
+     for (int i=0; i<length; i++){
+        int rand = (int) (Math.random() * passwordSet.length());
+        password[i] = passwordSet.charAt(rand);
+     }
+     return new String(password);
+   }
+
+    public void setMailBoxCapacity(int capacity, String type){
+    this.mailCapacity = capacity+type;
+   }
+
+    public void setAlternateMail(String email){
+    this.alternateEmail = email;
+   }
+
+    public String getEmail(){
+    return email;
+   }
+
+   public String getAlternateMail(){
+    return alternateEmail;
    }
 
 
+   public String getPassword(){
+    return password;
+   }
+
+   public String getMailCapacity(){
+    return mailCapacity;
+   }
+
+   public String getFullname(){
+    return firstname +" "+ lastname;
+   }
+
+   public void getProfile(){
+    Dictionary data = new Hashtable();
+    data.put("Full Name", getFullname());
+    data.put("Password", getPassword());
+    data.put("Email", getEmail());
+    data.put("Email Capacity", getMailCapacity());
+    data.put("Alternate Email", getAlternateMail());
+    System.out.println(data);
+   }
 }
